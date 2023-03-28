8efa921ebce46aff3a63f4d419b0ef52ce50bd06
lanbow93 <the.doctor1501@gmail.com>
2023-03-28 13:40:43 -0500

    Sign in has form validation and completed styling

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,7 +5,7 @@ export default function Login() {
     return <div className="formArea">
         <Form action="/login" method="post" className="userForm">
         <input type="text" name="username" placeholder="username"  />
-        <input type="text" name="password" placeholder="password" />
+        <input type="password" name="password" placeholder="password" />
         <button>Login</button>
     </Form>
     </div>
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,20 +1,40 @@
-
+import { useState } from "react"
 import {Form} from "react-router-dom"
+
+interface UserForm {
+    name: string,
+    username: string,
+    password: string,
+    confirmPassword: string
+}
 
 
 export default function Signup() {
+    const [formData, setFormData] = useState<UserForm>({
+        name: "",
+        username: "",
+        password: "",
+        confirmPassword: ""
+    })
+
     return <div className="formArea">
         <Form action="/signup" method="post" className="userForm">
         <h3>Create New User</h3>
+
         <label htmlFor="name" >Name:</label>
-        <input type="text" name="name" placeholder="name" />
+        <input type="text" name="name" value={formData.name} onChange={event => setFormData({...formData, name: event.target.value})} />
+
         <label htmlFor="username" >Username:</label>
-        <input type="text" name="username" placeholder="username"  />
+        <input type="text" name="username" value={formData.username} onChange={event => setFormData({...formData, username: event.target.value})} />
+
         <label htmlFor="name" >Password:</label>
-        <input type="password" name="password" placeholder="password" />
+        <input type="password" name="password" value={formData.password}  onChange={event => setFormData({...formData, password: event.target.value})}/>
+
         <label htmlFor="name" >Confirm Password:</label>
-        <input type="password" name="confirmpassword" placeholder="confirm password"  />
-        <button className="disabled" >Create User</button>
+        {formData.password === formData.confirmPassword ? <input type="password" name="confirmpassword" value={formData.confirmPassword} onChange={event => setFormData({...formData, confirmPassword: event.target.value})} /> : <input type="password" name="confirmpassword"  value={formData.confirmPassword} onChange={event => setFormData({...formData, confirmPassword: event.target.value})} className="redHighlight"/>}
+
+        
+        {formData.name && formData.confirmPassword && formData.password && formData.username && formData.password === formData.confirmPassword ?  <button className="ready" >Create User</button> :  <button className="disabled" disabled >Create User</button> }
         </Form>
     </div>
 }
